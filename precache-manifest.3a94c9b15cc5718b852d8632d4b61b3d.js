c9b64cb49e700c57844b279aba97b9c71c08f3a9
Nisarg <nisargvaghela2001@gmail.com>
2023-10-15 08:48:43 +0530

    Updates

---
+++ precache-manifest.3a94c9b15cc5718b852d8632d4b61b3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4cfedc0176f1aec35f46810e2f8871",
+    "url": "/quicksell-assignment/index.html"
+  },
+  {
+    "revision": "7f499f69429982c6a168",
+    "url": "/quicksell-assignment/static/css/main.59057a01.chunk.css"
+  },
+  {
+    "revision": "81a17e889575cc7f3ab4",
+    "url": "/quicksell-assignment/static/js/2.09905852.chunk.js"
+  },
+  {
+    "revision": "7f499f69429982c6a168",
+    "url": "/quicksell-assignment/static/js/main.fdd6eafe.chunk.js"
+  },
+  {
+    "revision": "ac15663b279a85e1a97c",
+    "url": "/quicksell-assignment/static/js/runtime~main.7b44d9b8.js"
+  },
+  {
+    "revision": "500bcc1a188182258dddf51a4bf857cc",
+    "url": "/quicksell-assignment/static/media/done.500bcc1a.png"
+  },
+  {
+    "revision": "6739b1d85a90a1384d3c4f353adace36",
+    "url": "/quicksell-assignment/static/media/in_progress.6739b1d8.jpg"
+  },
+  {
+    "revision": "35b6f4011db474a6732d0bef8144668f",
+    "url": "/quicksell-assignment/static/media/todo.35b6f401.png"
+  }
+]);
